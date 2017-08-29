a1ea79419fa97442cd0c81d0d4384df00cf77e04
yenkuanlee <yenkuanlee@gmail.com>
2017-08-29 10:28:31 +0800

    IPDC master include MR and TF

--- run_script.py
+++
@@ -1,4 +0,0 @@
-import os
-for i in range(100):
-	os.system("rm -rf ../149*")
-	os.system("python test.py")
--- scp.py
+++
@@ -1,4 +0,0 @@
-import os
-IpList = ['192.168.122.39','192.168.122.40','192.168.122.71','192.168.122.171']
-for x in IpList:
-	os.system("scp Dmqtt.py "+x+":/tmp")
