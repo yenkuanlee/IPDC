5a63fe6936d02c56ee492942295e27d803de7d1f
yenkuanlee <yenkuanlee@gmail.com>
2017-10-02 11:55:26 +0800

    ER bug fix

--- ER/Dmqtt.py
+++ ER/Dmqtt.py
@@ -40,7 +40,7 @@ def DownloadAndSetEnode(message,Eclient):
 	Fname = tmp[1]
 	os.system("timeout 10 ipfs get "+Fhash+" -o /tmp/"+Fname)
 	time.sleep(1)	
-	SEthread = threading.Thread(target=SetEnode, name="SetEnodeAfterDownload", args=Eclient)
+	SEthread = threading.Thread(target=SetEnode, name="SetEnodeAfterDownload", args=(Eclient,))
 	SEthread.setDaemon = True
 	SEthread.start()
             
