fba9cfe69b825d7eaeeea2123a28b29cad4cdd0f
Kevin Yen-Kuan Lee <yenkuanlee@gmail.com>
2018-03-27 11:04:08 +0800

    ER Dmqtt modify for python3 because web3 not support for python2 anymore ...

--- ER/Dmqtt.py
+++ ER/Dmqtt.py
@@ -11,17 +11,17 @@ DbPath = "/tmp/.db"
 
 # The callback for when the client receives a CONNACK response from the server.
 def on_connect(client, userdata,flags_dict, rc):
-	client.subscribe("test")
-	client.subscribe("AskResource")
-	client.subscribe("RunningChain")
-	client.subscribe("StopChain")
-	client.subscribe("DownloadAndSetEnode")
-	client.subscribe("SetEnode")
-	client.subscribe("AddPeer")
-        client.subscribe("KeyStore")
-	client.subscribe("CloseEnode")
-	client.subscribe("CleanUp")
-	client.subscribe("PortalConnect")
+    client.subscribe("test")
+    client.subscribe("AskResource")
+    client.subscribe("RunningChain")
+    client.subscribe("StopChain")
+    client.subscribe("DownloadAndSetEnode")
+    client.subscribe("SetEnode")
+    client.subscribe("AddPeer")
+    client.subscribe("KeyStore")
+    client.subscribe("CloseEnode")
+    client.subscribe("CleanUp")
+    client.subscribe("PortalConnect")
 
 def Publish(target,channel,message):
 	client = mqtt.Client()
@@ -29,46 +29,46 @@ def Publish(target,channel,message):
 	client.connect(target, 1883)
 	(rc, mid) = client.publish(channel, message, qos=1)
 	#time.sleep(0.01)
-	print "DMQTT RESULT : "+str(rc)
+	print("DMQTT RESULT : "+str(rc))
 
 def SetEnode(Eclient):
-	print "Setting Enode"
+	print("Setting Enode")
 	cmd = "python /tmp/enode_setting.py"
 	try:
 		p = Popen(cmd.split())
 		Eclient.WorkerPID = str(p.pid)
 	except Exception as e:
-		print "ENODE SETTING ERROR"
+		print("ENODE SETTING ERROR")
 
 def RunVigilante(Eclient):
-        print "Run Vigilante"
+        print("Run Vigilante")
         cmd = "python /tmp/LocalVigilante.py"
         try:
                 p = Popen(cmd.split())
                 Eclient.VigilantePID = str(p.pid)
         except Exception as e:
-                print "ENODE SETTING ERROR"
+                print("ENODE SETTING ERROR")
 		
 def DownloadAndSetEnode(message,Eclient):
-	# format : Fhash###Fname
-	print "CallDownload : "+message
-	tmp = message.split("###")
-	Ehash = tmp[0]
-	Ohash = tmp[1]
-        Vhash = tmp[2]
-	os.system("timeout 10 ipfs get "+Ehash+" -o /tmp/enode_setting.py")
-	time.sleep(1)
-	os.system("timeout 10 ipfs get "+Ohash+" -o /tmp/Ohash")
-	time.sleep(1)
-        os.system("timeout 10 ipfs get "+Vhash+" -o /tmp/LocalVigilante.py")
-        time.sleep(1)
-	SEthread = threading.Thread(target=SetEnode, name="SetEnodeAfterDownload", args=(Eclient,))
-	SEthread.setDaemon = True
-	SEthread.start()
-        # Should run LocalVigilante here
-        Vthread = threading.Thread(target=RunVigilante, name="RunVigilanteAfterDownload", args=(Eclient,))
-        Vthread.setDaemon = True
-        Vthread.start()
+    # format : Fhash###Fname
+    print("CallDownload : "+message)
+    tmp = message.split("###")
+    Ehash = tmp[0]
+    Ohash = tmp[1]
+    Vhash = tmp[2]
+    os.system("timeout 10 ipfs get "+Ehash+" -o /tmp/enode_setting.py")
+    time.sleep(1)
+    os.system("timeout 10 ipfs get "+Ohash+" -o /tmp/Ohash")
+    time.sleep(1)
+    os.system("timeout 10 ipfs get "+Vhash+" -o /tmp/LocalVigilante.py")
+    time.sleep(1)
+    SEthread = threading.Thread(target=SetEnode, name="SetEnodeAfterDownload", args=(Eclient,))
+    SEthread.setDaemon = True
+    SEthread.start()
+    # Should run LocalVigilante here
+    Vthread = threading.Thread(target=RunVigilante, name="RunVigilanteAfterDownload", args=(Eclient,))
+    Vthread.setDaemon = True
+    Vthread.start()
 
 def LoadDescription():
     Ddict = dict()
@@ -79,8 +79,8 @@ def LoadDescription():
                 break
         tmp = line.split("=")
         for i in range(len(tmp)):
-		if tmp[0] == "description":
-			continue
+            if tmp[0] == "description":
+                continue
                 tmp[i] = tmp[i].replace(" ","")
                 tmp[i] = tmp[i].replace("\n","")
                 tmp[i] = tmp[i].replace("\t","")
@@ -89,7 +89,7 @@ def LoadDescription():
     return Ddict
 
 def AskResource(message):
-	print "AskResource : "+message
+	print("AskResource : "+message)
 	Ddict = json.loads(message)
 	# Setup database
 	os.system("mkdir -p "+DbPath)
@@ -101,7 +101,7 @@ def AskResource(message):
 	conn.commit()
 
 def RunningChain(message):
-	print "RunningChain : "+message
+	print("RunningChain : "+message)
 	conn = sqlite3.connect(DbPath+"/chain.db")
 	c = conn.cursor()
 	c.execute("create table if not exists RunningChain(Ohash text)")
@@ -121,94 +121,93 @@ def RunningChain(message):
 	conn.commit()
 
 def StopChain(message):
-	print "StopChain : "+message
+	print("StopChain : "+message)
 	conn = sqlite3.connect(DbPath+"/chain.db")
 	c = conn.cursor()
 	c.execute("delete from RunningChain where Ohash = '"+message+"'")
 	conn.commit()
 
 def AddPeer(message):
-	print "AddPeer : "+message
+	print("AddPeer : "+message)
 	from web3 import Web3, HTTPProvider
 	web3 = Web3(HTTPProvider('http://localhost:8545'))
 	try:
 		web3.admin.addPeer(message)
 	except:
-		print "Add Peer Fail!!!"
+		print("Add Peer Fail!!!")
 
 def KeyStore(message):
-    print "KeyStore : "+message
+    print("KeyStore : "+message)
     tmp = message.split("###")
     try:
         peerID = tmp[0]
         Kname = tmp[1]
-	Khash = tmp[2]
+        Khash = tmp[2]
     except:
-        print "ERROR!!!"
-	return
+        print("ERROR!!!")
+        return
     conn = sqlite3.connect(DbPath+"/chain.db")
     c = conn.cursor()
     try:
-	c.execute("create table if not exists keystore(peerID text, Kname text,Khash text, PRIMARY KEY(peerID,Kname))")
-	conn.commit()
-	c.execute("INSERT OR REPLACE INTO keystore values('"+peerID+"','"+Kname+"','"+Khash+"')")
-	conn.commit()
+        c.execute("create table if not exists keystore(peerID text, Kname text,Khash text, PRIMARY KEY(peerID,Kname))")
+        conn.commit()
+        c.execute("INSERT OR REPLACE INTO keystore values('"+peerID+"','"+Kname+"','"+Khash+"')")
+        conn.commit()
         os.system("timeout 100 ipfs pin add "+Khash)
     except:
-	print "Keystore Upsert Error!!!"
+        print("Keystore Upsert Error!!!")
 
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
-	if msg.topic=='test':
-		print str(msg.payload)
-	elif msg.topic=="DownloadAndSetEnode":
-		DownloadAndSetEnode(str(msg.payload),client)
-	elif msg.topic=="SetEnode":
-		SEthread = threading.Thread(target=SetEnode, name="SetEnode", args=(client))
-		SEthread.setDaemon = True
-		SEthread.start()
-	elif msg.topic=="AskResource":
-		AskResource(str(msg.payload))
-	elif msg.topic=="RunningChain":
-		RunningChain(str(msg.payload))
-	elif msg.topic=="StopChain":
-		StopChain(str(msg.payload))
-	elif msg.topic=="AddPeer":
-		AddPeer(str(msg.payload))
-        elif msg.topic=="KeyStore":
-                KeyStore(str(msg.payload))
-	elif msg.topic=="CloseEnode":
-		os.system("kill -9 "+client.WorkerPID)
-                os.system("kill -9 "+client.VigilantePID)
-		print "KEVIN KILLED "+client.WorkerPID
-		client.WorkerPID = ""
-                client.VigilantePID = ""
-		os.system("killall -9 geth")
-                os.system("rm -rf /tmp/createChain")
-                #os.system("rm -rf "+DbPath)
-	elif msg.topic=="PortalConnect":
-		ConnectIpList = str(msg.payload).split("###")
-		for x in ConnectIpList:
-			if x == "":continue
-			cmd = "ipfs swarm connect "+x
-			try:
-				cmd = "ipfs id -f='<id>'"
-				peerID = subprocess.check_output(cmd, shell=True)
-				if peerID in x: # Can't connect to himself
-					print "\n\nWelcome to be a Domain Portal.\nPlease press Enter!"
-					continue
-				cmd = "ipfs swarm connect "+x
-				output = subprocess.check_output(cmd, shell=True)
-				if "success" in output:
-					RemoteIP = x.split("/")[2]
-					Publish(RemoteIP,"test","\n\nSuccess to connect with Portal.\nPlease press Enter!")
-					break
-			except:
-				pass
-	elif msg.topic=="CleanUp":
-		os.system("rm Map.py* Reduce.py* output.txt data.dat")
-	#time.sleep(0.01)
+    if msg.topic=='test':
+        print(str(msg.payload))
+    elif msg.topic=="DownloadAndSetEnode":
+        DownloadAndSetEnode(str(msg.payload),client)
+    elif msg.topic=="SetEnode":
+        SEthread = threading.Thread(target=SetEnode, name="SetEnode", args=(client))
+        SEthread.setDaemon = True
+        SEthread.start()
+    elif msg.topic=="AskResource":
+        AskResource(str(msg.payload))
+    elif msg.topic=="RunningChain":
+        RunningChain(str(msg.payload))
+    elif msg.topic=="StopChain":
+        StopChain(str(msg.payload))
+    elif msg.topic=="AddPeer":
+        AddPeer(str(msg.payload))
+    elif msg.topic=="KeyStore":
+        KeyStore(str(msg.payload))
+    elif msg.topic=="CloseEnode":
+        os.system("kill -9 "+client.WorkerPID)
+        os.system("kill -9 "+client.VigilantePID)
+        print("KEVIN KILLED "+client.WorkerPID)
+        client.WorkerPID = ""
+        client.VigilantePID = ""
+        os.system("killall -9 geth")
+        os.system("rm -rf /tmp/createChain")
+    elif msg.topic=="PortalConnect":
+        ConnectIpList = str(msg.payload).split("###")
+        for x in ConnectIpList:
+            if x == "":continue
+            cmd = "ipfs swarm connect "+x
+            try:
+                cmd = "ipfs id -f='<id>'"
+                peerID = subprocess.check_output(cmd, shell=True)
+                if peerID in x: # Can't connect to himself
+                    print("\n\nWelcome to be a Domain Portal.\nPlease press Enter!")
+                    continue
+                cmd = "ipfs swarm connect "+x
+                output = subprocess.check_output(cmd, shell=True)
+                if "success" in output:
+                    RemoteIP = x.split("/")[2]
+                    Publish(RemoteIP,"test","\n\nSuccess to connect with Portal.\nPlease press Enter!")
+                    break
+            except:
+                pass
+    elif msg.topic=="CleanUp":
+        os.system("rm Map.py* Reduce.py* output.txt data.dat")
+        #time.sleep(0.01)
 
 client = mqtt.Client()
 client.WorkerPID = ""
